8bf58c981a33cbdf0b67a3f8a4b47b937195f2e9
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-22 07:25:42 -0700

    Added the file print_int.c

---
+++ print_int.c
@@ -0,0 +1,20 @@
+/*
+ * Author: 0xTariq-dev, Mostafa1Jamal1
+ * File: print_int.c
+ */
+
+#include "main.h"
+
+/**
+ * print_int - Prints 
+ * @n: The value
+ * Return: Always 0.
+ */
+int print_int(const char *format, ...)
+{
+	if (format == %)
+	{
+		while ((format + 1) == 
+
+	return ();
+}
