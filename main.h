8fcda4f4fe642eb7502c9fb412fa614b3a7253e5
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-22 06:58:17 -0700

    Added the main.h file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/*
+ * file: main.h
+ * author: 0xTariq-dev, Mostafa1Jamal1
+ * describtion: Header file contains declarations for functions
+ * 		used in the ALX printf project
+ */
+
+int _putchar(char c);
+
+#endif
