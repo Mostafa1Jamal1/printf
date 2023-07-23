dedc10f61cf5db7a5e8198c10da1a67d9be866e4
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-22 18:55:17 -0700

    Added README.md file

--- print_int.c
+++ print_int.c
@@ -13,5 +13,34 @@
 int print_int(va_list arg)
 {
 	int n = va_arg(arg, int);
-	unsigned int x;
+	unsigned int *x;
+	int count = 0, div = 1, i = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else
+		n;
+
+	for (; n / div >= 10; i++)
+		div *= 10
+
+	count = i;
+	x = malloc((sizeof(char) * count) + 1);
+
+	for (i = 0; div != 0; i++)
+	{
+		x[i] = (n %= div);
+		div /= 10;
+	}
+	for (i = 0; i <= count; i++)
+	{
+		_putchar(x[i] + '0');
+	}
+	free(x);
+	return (count);
+}
+		
 
